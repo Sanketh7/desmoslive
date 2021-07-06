2a5fe8a39c840923ac255e2a0d389b11d18b5373
Sanketh Varamballi <sankethv03@gmail.com>
2021-07-05 23:18:54 -0700

    cleaned up login component

--- packages/web/src/components/Login/Login.tsx
+++ packages/web/src/components/Login/Login.tsx
@@ -1,51 +1,35 @@
 import {
+  GoogleLogin,
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
-  useGoogleLogin,
 } from "react-google-login";
-
-const clientId = process.env.REACT_APP_GCLOUD_OAUTH_CLIENT_ID || "";
-
-const refreshTokenSystem = (res: GoogleLoginResponse) => {
-  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
-  const refreshToken = async () => {
-    const newAuth = await res.reloadAuthResponse();
-    refreshTiming = (newAuth.expires_in || 3600 - 5 * 60) * 1000;
-    setTimeout(refreshToken, refreshTiming);
-  };
-  setTimeout(refreshToken, refreshTiming);
-};
+import axios from "axios";
 
 type LoginResponse = GoogleLoginResponse | GoogleLoginResponseOffline;
+const isOnlineResponse = (res: LoginResponse): res is GoogleLoginResponse => {
+  return (res as GoogleLoginResponse).tokenId !== undefined;
+};
 
-const isOnlineResponse = (res: LoginResponse): res is GoogleLoginResponse =>
-  (res as GoogleLoginResponse).tokenId !== undefined;
-
-const Login = (): JSX.Element => {
-  const onSuccess = (res: LoginResponse) => {
-    if (isOnlineResponse(res)) {
-      console.log(res.profileObj);
-      console.log();
-      refreshTokenSystem(res);
+const Login: React.FC = () => {
+  const handleLogin = async (data: LoginResponse) => {
+    if (isOnlineResponse(data)) {
+      try {
+        const res = await axios.post("http://localhost:7000", {
+          token: data.tokenId,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onFailure = (res: any) => {
-    console.log("Login failed.", res);
-  };
-
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId,
-    isSignedIn: true,
-  });
-
   return (
-    <button onClick={signIn}>
-      <span>Sign in with Google</span>
-    </button>
+    <GoogleLogin
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}
+      buttonText="Log in with Google"
+      onSuccess={handleLogin}
+      onFailure={handleLogin}
+      cookiePolicy="single-host-origin"
+    />
   );
 };
 
