3b9bece9e1a7be45f279f57f25fdad2b98606261
Sanketh Varamballi <sankethv03@gmail.com>
2021-07-07 23:35:51 -0700

    added graph and user mongoose models

---
+++ packages/api/src/models/graph.model.ts
@@ -0,0 +1,26 @@
+import { Document, model, Schema, Types } from "mongoose";
+import { UserDocument } from "./user.model";
+
+interface Graph {
+    name: string;
+    owner: Types.ObjectId | UserDocument;
+    sharedWith: Types.ObjectId[] | UserDocument[];
+}
+
+// eslint-disable-next-line prettier/prettier
+interface GraphDocument extends Document, Graph { }
+
+const GraphSchema = new Schema({
+    name: { type: String, required: true },
+    owner: { type: Types.ObjectId, ref: "User", required: true },
+    sharedWith: [
+        {
+            type: Types.ObjectId,
+            ref: "User",
+        },
+    ],
+});
+
+const GraphModel = model<GraphDocument>("Graph", GraphSchema);
+
+export { Graph, GraphDocument, GraphModel };
--- packages/api/src/models/user.model.ts
+++ packages/api/src/models/user.model.ts
@@ -1,15 +1,32 @@
-import { Schema, Model, model } from "mongoose";
+import { Schema, Model, model, Document, Types } from "mongoose";
 
 interface User {
     name: string;
     email: string;
+    myGraphs: Types.ObjectId[] | UserDocument[];
+    sharedGraphs: Types.ObjectId[] | UserDocument[];
 }
 
-const schema: Schema = new Schema({
+// eslint-disable-next-line prettier/prettier
+interface UserDocument extends Document, User { }
+
+const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    myGraphs: [
+        {
+            type: Types.ObjectId,
+            ref: "Graph",
+        },
+    ],
+    sharedGraphs: [
+        {
+            type: Types.ObjectId,
+            ref: "Graph",
+        },
+    ],
 });
 
-const UserModel: Model<User> = model<User>("User", schema);
+const UserModel: Model<UserDocument> = model<UserDocument>("User", UserSchema);
 
-export { User, UserModel };
+export { User, UserDocument, UserModel };
