9aa10038ff265bbdd72111f7a283c0b608fd59f5
Sanketh Varamballi <sankethv03@gmail.com>
2021-08-06 21:54:01 -0700

    added branch selection warning

--- packages/web/src/components/Editor/Calculator/BranchSelect.tsx
+++ packages/web/src/components/Editor/Calculator/BranchSelect.tsx
@@ -1,4 +1,5 @@
 import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import { useBranchesDataSWR, useMyBranchIDSWR } from "../../../api/swrRequests";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { setActiveBranch } from "../../../redux/slices/activeBranchSlice";
@@ -26,37 +27,42 @@ export const BranchSelect: React.FC = () => {
   */
 
   return (
-    <div style={{ display: "flex" }}>
-      <FormControl variant="outlined" style={{ flex: "auto" }}>
-        <InputLabel id="select-labe">Branch</InputLabel>
-        <Select
-          labelId="select-label"
-          id="select"
-          value={activeBranch.id}
-          onChange={(event) =>
-            dispatch(
-              setActiveBranch({
-                id: event.target.value as string,
-                isOwner: Boolean(
-                  myBranchID && event.target.value === myBranchID
-                ),
-              })
-            )
-          }
-          label="Branch"
-        >
-          {branches &&
-            branches.map((branch) => {
-              return (
-                <MenuItem key={branch.id} value={branch.id}>
-                  {branch.owner.email +
-                    (branch.owner.email === authEmail ? " (me)" : "")}
-                </MenuItem>
-              );
-            })}
-        </Select>
-      </FormControl>
-      <MergeButton />
+    <div>
+      <div style={{ display: "flex" }}>
+        <FormControl variant="outlined" style={{ flex: "auto" }}>
+          <InputLabel id="select-labe">Branch</InputLabel>
+          <Select
+            labelId="select-label"
+            id="select"
+            value={activeBranch.id}
+            onChange={(event) =>
+              dispatch(
+                setActiveBranch({
+                  id: event.target.value as string,
+                  isOwner: Boolean(
+                    myBranchID && event.target.value === myBranchID
+                  ),
+                })
+              )
+            }
+            label="Branch"
+          >
+            {branches &&
+              branches.map((branch) => {
+                return (
+                  <MenuItem key={branch.id} value={branch.id}>
+                    {branch.owner.email +
+                      (branch.owner.email === authEmail ? " (me)" : "")}
+                  </MenuItem>
+                );
+              })}
+          </Select>
+        </FormControl>
+        <MergeButton />
+      </div>
+      {!activeBranch.id && (
+        <Alert severity="warning">You need to select a branch!</Alert>
+      )}
     </div>
   );
 };
